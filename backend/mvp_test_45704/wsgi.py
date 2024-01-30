ab5560f3da559b593e456deff9facf1476146fc9
Crowdbotics <team@crowdbotics.com>
2024-01-30 16:50:36 +0000

    Initial commit

---
+++ backend/mvp_test_45704/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for mvp_test_45704 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "mvp_test_45704.settings"
+)
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "mvp-test-45704",
+  url: "https://mvp-test-45704.botics.co",
+  api: "https://mvp-test-45704.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
